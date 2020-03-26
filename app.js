b56bddfb26592d5ed4fd64a4215e0e38e2019aeb
Haritha Gunda <gunda.haritha@gmail.com>
2020-03-25 21:52:42 -0400

    Express session and saving the session in Server File store (Server hard disk)

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const createError=require('http-errors');
 const express=require('express');
 const path=require('path');
-const cookieParser=require('cookie-parser');
+//const cookieParser=require('cookie-parser');
 const logger=require('morgan');
 const mongoose=require('mongoose');
+const session=require('express-session');
+const FileStore=require('session-file-store')(session);
 
 const url='mongodb://localhost:27017/nucampsite';
 const connect=mongoose.connect(url, {
@@ -32,10 +34,18 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 function auth(req, res, next) {
-  if (!req.signedCookies.user) {
+  console.log(req.session);
+  if (!req.session.user) {
     const authHeader=req.headers.authorization;
     if (!authHeader) {
       const err=new Error('You are not authenticated!');
@@ -48,7 +58,7 @@ function auth(req, res, next) {
     const user=auth[0];
     const pass=auth[1];
     if (user==='admin'&&pass==='password') {
-      res.cookie('user', 'admin', {signed: true});
+      req.session.user='admin';
       return next(); // authorized
     } else {
       const err=new Error('You are not authenticated!');
@@ -57,7 +67,8 @@ function auth(req, res, next) {
       return next(err);
     }
   } else {
-    if (req.signedCookies.user==='admin') {
+    if (req.session.user==='admin') {
+      console.log('req.session:', req.session);
       return next();
     } else {
       const err=new Error('You are not authenticated!');
@@ -69,8 +80,8 @@ function auth(req, res, next) {
 
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
