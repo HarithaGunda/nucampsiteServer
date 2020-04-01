321b64bbae9c4a2badb4af32fa7f6054b49544b9
Haritha Gunda <gunda.haritha@gmail.com>
2020-03-31 22:10:06 -0400

    Added CORS Node module and a custome cors method to configue whitelist, sent preflight messages before actual req, cors middleware to respond with the correct headers to cross-origin requests

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports={
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '3863769870363243',
+        clientSecret: '6048e523260991ba6007f79e4b919aca'
+    }
 }
